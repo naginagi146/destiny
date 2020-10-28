e742b8dda9dfcbc2c66c584df4e8a98b430fdc4b
naginagi146 <naginagi146@gmail.com>
2020-10-28 21:55:33 +0900

    1029

--- destiny/urls.py
+++ destiny/urls.py
@@ -5,5 +5,10 @@ app_name= 'destiny'
 
 urlpatterns = [
     path('', views.RouletteListView.as_view(), name='roulette_list'),
+    path('detail/<int:pk>/', views.RouletteDetailView.as_view(), name='roulette_detail'),
+    path('random/<int:pk>/', views.RandomRouletteView.as_view(), name='random'),
+    path('result/<int:pk>/', views.ResultView.as_view(), name='result'),
     path('create/', views.RouletteCreateView.as_view(), name='roulette_create'),
+    path('update/<int:pk>/', views.RouletteUpdateView.as_view(), name='roulette_update'),
+
 ]
